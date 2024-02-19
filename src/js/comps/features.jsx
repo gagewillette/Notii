640efeb49458cf26ff146cf40738f936a2b33327
gagewillette <79120652+gagewillette@users.noreply.github.com>
2024-02-18 23:15:52 -0500

    features done

--- src/js/comps/features.jsx
+++ src/js/comps/features.jsx
@@ -1,13 +1,35 @@
 import React from "react";
 import "../../styles/features.scss";
+import cloud from "../../assets/cloud_FILL0_wght400_GRAD0_opsz24.svg"
 
 export default function Features() {
   return (
-    <div className="grid-container">
-      <div className="grid-item">Div 1</div>
-      <div className="grid-item">Div 2</div>
-      <div className="grid-item">Div 3</div>
-      <div className="grid-item">Div 4</div>
-    </div>
+    <>
+      <div className="features-wrapper">
+        <div className="features-text">
+          <h3>Features</h3>
+          <h2>Powerful Features that Enhance Your Note-Taking Experience</h2>
+        </div>
+
+        <div className="grid-wrapper">
+          <div className="grid-container">
+            <div className="grid-item">
+              <div className="icon"><img src={cloud} width={40} /></div>
+              <div className="text">Cloud storage Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis dolorum repellat ipsa, officiis eveniet consequuntur quis odio, eligendi ex neque ullam, mollitia laudantium? Asperiores odio veritatis, perferendis ut deleniti enim.
+              </div>
+            </div>
+            <div className="grid-item"><div className="icon"><img src={cloud} width={40} /></div>
+              <div className="text">Cloud storage Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis dolorum repellat ipsa, officiis eveniet consequuntur quis odio, eligendi ex neque ullam, mollitia laudantium? Asperiores odio veritatis, perferendis ut deleniti enim.
+              </div></div>
+            <div className="grid-item"><div className="icon"><img src={cloud} width={40} /></div>
+              <div className="text">Cloud storage Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis dolorum repellat ipsa, officiis eveniet consequuntur quis odio, eligendi ex neque ullam, mollitia laudantium? Asperiores odio veritatis, perferendis ut deleniti enim.
+              </div></div>
+            <div className="grid-item"><div className="icon"><img src={cloud} width={40} /></div>
+              <div className="text">Cloud storage Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis dolorum repellat ipsa, officiis eveniet consequuntur quis odio, eligendi ex neque ullam, mollitia laudantium? Asperiores odio veritatis, perferendis ut deleniti enim.
+              </div></div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
