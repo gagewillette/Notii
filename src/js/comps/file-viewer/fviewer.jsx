dea4fad62b9f1ff716739f6545e971e7b5fc4e28
gagewillette <79120652+gagewillette@users.noreply.github.com>
2024-05-14 20:54:46 -0400

    conditioanl rendering fix and styling

--- src/js/comps/file-viewer/fviewer.jsx
+++ src/js/comps/file-viewer/fviewer.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 import Note from "./note";
 import "../../../styles/fileviewer.scss";
-import { deleteNote, addNote  } from "../../../backend/db/noteservice";
+import { deleteNote, addNote } from "../../../backend/db/noteservice";
+import { Sidebar } from "react-pro-sidebar";
 
-
-export default function FileViewer({notes, setSelectedNote}) {
+export default function FileViewer({ notes, setSelectedNote }) {
   const [isMakingNewNote, setIsMakingNewNote] = useState(false);
   const [newNoteTitle, setNewNoteTitle] = useState("");
-
+  const [collapsed, setCollapsed] = useState(false);
 
   const handleSaveNote = () => {
     //add note to db
@@ -18,7 +18,6 @@ export default function FileViewer({notes, setSelectedNote}) {
 
     setIsMakingNewNote(false);
   };
-
 
   const handleCancelNote = () => {
     setIsMakingNewNote(false);
@@ -32,39 +31,56 @@ export default function FileViewer({notes, setSelectedNote}) {
     deleteNote(id);
   };
 
+  const handleToggleSidebar = () => {
+    setCollapsed(!collapsed);
+  };
+
   return (
-    <div className="file-viewer">
-      <div className="file-viewer-header">
-        <h1>Notes</h1>
-        <button onClick={AddNote}>+</button>
-      </div>
-
-      <ul>
-        {isMakingNewNote && (
-          <div className="new-note">
-            <div className="new-note-fields">
-              <input
-                type="text"
-                placeholder="Note Title"
-                onChange={(e) => {
-                  setNewNoteTitle(e.target.value);
-                }}
-              />
-            </div>
-            <div className="new-note-buttons">
-              <button onClick={handleSaveNote}>Save</button>
-              <button onClick={handleCancelNote}>Cancel</button>
-            </div>
+    <>
+      <Sidebar collapsed={collapsed}>
+        <div className="file-viewer">
+          <div className="file-viewer-header">
+            <h1>Notes</h1>
+            <button onClick={AddNote}>+</button>
           </div>
-        )}
 
-        {notes.map((note) => (
-          <li key={note.id}>
-            <Note note={note} onDelete={handleDeleteNote} setSelectedNote={setSelectedNote} />
-          </li>
-        ))}
-      </ul>
-    </div>
+          <div className="sidebar-button">
+            <button onClick={handleToggleSidebar}>
+              {collapsed ? ">" : "<"}
+            </button>
+          </div>
+
+          <ul>
+            {isMakingNewNote && (
+              <div className="new-note">
+                <div className="new-note-fields">
+                  <input
+                    type="text"
+                    placeholder="Note Title"
+                    onChange={(e) => {
+                      setNewNoteTitle(e.target.value);
+                    }}
+                  />
+                </div>
+                <div className="new-note-buttons">
+                  <button onClick={handleSaveNote}>Save</button>
+                  <button onClick={handleCancelNote}>Cancel</button>
+                </div>
+              </div>
+            )}
+
+            {notes.map((note) => (
+              <li key={note.id}>
+                <Note
+                  note={note}
+                  onDelete={handleDeleteNote}
+                  setSelectedNote={setSelectedNote}
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </Sidebar>
+    </>
   );
 }
-
